0b807687544559fb387a6a7e9ce23764fe6e7ac5
AlphaTech000 <chavalpet.petoton@gmail.com>
2025-05-13 19:10:31 +0200

    Alpha 1.0

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = ''
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://beyondsurvival.duckdns.org/MODPACK1.0.0.mrpack'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
